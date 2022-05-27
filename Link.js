8f06fcc69901685c07f56555d4f442a55cab6ebe
r119310 <r119310@g.kobe-kosen.ac.jp>
2022-05-27 12:01:03 +0900

    第2回ファイル名変更の会

--- Link.js
+++ Link.js
@@ -1,7 +1,7 @@
 function enLink() {
-    window.location.href = "https://r119310.github.io/ENPage/index.html";
+    window.location.href = "https://r119310.github.io/MainPage/EN/index.html";
 }
 
 function jpLink() {
-    window.location.href = "https://r119310.github.io/JPPage/index.html";
+    window.location.href = "https://r119310.github.io/MainPage/JP/index.html";
 }
