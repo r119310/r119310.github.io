c4f044f77554639e19ab047e79aa9e432da15b1e
r119310 <r119310@g.kobe-kosen.ac.jp>
2022-05-28 23:39:40 +0900

    小変更 004

--- Link.js
+++ Link.js
@@ -1,7 +1,7 @@
 function enLink() {
-    window.location.href = "https://r119310.github.io/MainPage/EN/index.html";
+    window.location.href = "https://r119310.github.io/Portfolio/EN/index.html";
 }
 
 function jpLink() {
-    window.location.href = "https://r119310.github.io/MainPage/JP/index.html";
+    window.location.href = "https://r119310.github.io/Portfolio/JP/index.html";
 }
