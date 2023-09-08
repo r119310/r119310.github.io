f4af1cc8a4fdbcb438e0b0e8b2d772f680e68687
r119310 <r119310@g.kobe-kosen.ac.jp>
2023-09-08 09:02:49 +0900

    disk menu not worling well

--- desktop/home/jp/index.php
+++ desktop/home/jp/index.php
@@ -6,19 +6,38 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./index.css">
     <title>🏠Home | Makoto Otsuka</title>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Shrikhand&display=swap');
+    </style>
 </head>
 
 <body>
     <main>
         <div class="lower-circle">
-            <div>
-                <div class="About">
-                    
+            <div class="fan-tile">
+                <a href="../en/index.php"  class="About-fan fan">
+                    <div class="buttonWords">about</div>
+                </a>
+                <!-- <div class="Play-fan">
+                    <a href="../en/index.php" class="GTpage">play</a>
                 </div>
+                <div class="Works-fan">
+                    <a href="../en/index.php" class="GTpage">works</a>
+                </div>
+                <div class="News-fan">
+                    <a href="../en/index.php" class="GTpage">news</a>
+                </div>
+                <div class="Contact-fan">
+                    <a href="../en/index.php" class="GTpage">contact</a>
+                </div>
+                <div class="Biography-fan">
+                    <a href="../en/index.php" class="GTpage">bio</a>
+                </div> -->
             </div>
-            <div class="upper-circle">
-            
-            </div>
+            <!-- <div class="upper-circle">
+                <h2><div class="Initial">O</div>tsuka<div class="Initial">M</div>akoto</h2>
+            </div> -->
         </div>
     </main>
 </body>
--- index.php
+++ index.php
@@ -8,55 +8,58 @@
     <title>Portfolio | Makoto Otsuka</title>
 </head>
 
-<body>
-    <main>
-    <?php
-        function getDeviceType() {
-            $userAgent = $_SERVER['HTTP_USER_AGENT'];
-            $deviceTypes = array(
-                'mobile' => array('Android', 'iPhone', 'iPad', 'Windows Phone', 'BlackBerry', 'Nokia'),
-                'tablet' => array('iPad', 'Android'),
-                'desktop' => array('Windows', 'Macintosh', 'Linux')
-            );
-        
-            foreach ($deviceTypes as $deviceType => $devices) {
-                foreach ($devices as $device) {
-                    if (strpos($userAgent, $device) !== false) {
-                        return $deviceType;
-                    }
+<?php
+    function getDeviceType() {
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        $deviceTypes = array(
+            'mobile' => array('Android', 'iPhone', 'iPad', 'Windows Phone', 'BlackBerry', 'Nokia'),
+            'tablet' => array('iPad', 'Android'),
+            'desktop' => array('Windows', 'Macintosh', 'Linux')
+        );
+
+        foreach ($deviceTypes as $deviceType => $devices) {
+            foreach ($devices as $device) {
+                if (strpos($userAgent, $device) !== false) {
+                    return $deviceType;
                 }
             }
-        
-            return 'unknown';
         }
 
-        $language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+        return 'unknown';
+    }
 
-        $deviceType = getDeviceType();
-        
-        switch ($deviceType) {
-            case 'mobile':
-                $deviceDerect = 'mobile/';
-                break;
-            case 'tablet':
-                $deviceDerect = 'tablet/';
-                break;
-            case 'desktop':
-                $deviceDerect = 'desktop/';
-                break;
-            default:
-                echo 'Unknown device type.';
-                break;
-        }
+    $language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
-        if ($language === 'ja') {
-            $langDerect = 'jp/';
-        } else {
-            $langDerect = 'en/';
-        }
+    $deviceType = getDeviceType();
 
-        $redirectUrl ='./'.$deviceDerect.'home/'.$langDerect.'index.php';
+    switch ($deviceType) {
+        case 'mobile':
+            $deviceDerect = 'mobile/';
+            break;
+        case 'tablet':
+            $deviceDerect = 'tablet/';
+            break;
+        case 'desktop':
+            $deviceDerect = 'desktop/';
+            break;
+        default:
+            echo 'Unknown device type.';
+            break;
+    }
 
+    if ($language === 'ja') {
+        $langDerect = 'jp/';
+    } else {
+        $langDerect = 'en/';
+    }
+
+    $redirectUrl ='./'.$deviceDerect.'home/'.$langDerect.'index.php';
+    ?>
+
+<body>
+    <main>
+    
+    <?php
         header("Location: $redirectUrl");
         exit;
     ?>
