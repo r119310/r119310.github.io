0e64c96d5cc4b94b2a790e8399e25df8e4e83151
r119310 <r119310@g.kobe-kosen.ac.jp>
2023-09-08 21:22:01 +0900

    made header

---
+++ CSS/Link.js
@@ -0,0 +1,27 @@
+function homeJPLink() {
+    window.location.href = "https://r119310.github.io/JP/home.html";
+}
+
+function aboutJPLink() {
+    window.location.href = "https://r119310.github.io/JP/about.html";
+}
+
+function newsJPLink() {
+    window.location.href = "https://r119310.github.io/JP/news.html";
+}
+
+function Mac020kLink() {
+    window.location.href = "";
+}
+
+function homeENLink() {
+    window.location.href = "https://r119310.github.io/EN/home.html";
+}
+
+function aboutENLink() {
+    window.location.href = "https://r119310.github.io/EN/about.html";
+}
+
+function newsJPLink() {
+    window.location.href = "https://r119310.github.io/EN/news.html";
+}
