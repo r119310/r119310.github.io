96b0071b6f9b75ab2418bc5c06dbb9d7588f65a9
r119310 <r119310@g.kobe-kosen.ac.jp>
2023-09-08 14:05:38 +0900

    re-arranged header

--- Link.js
+++
@@ -1,7 +0,0 @@
-function enLink() {
-    window.location.href = "https://r119310.github.io/Portfolio/EN/index.html";
-}
-
-function jpLink() {
-    window.location.href = "https://r119310.github.io/Portfolio/JP/index.html";
-}
